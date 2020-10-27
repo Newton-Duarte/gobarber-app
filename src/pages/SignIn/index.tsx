68beb106b0c3fd68618929f8f9f968fdcfc7ac0d
Newton Duarte <newton_@hotmail.com>
2020-10-27 07:42:48 -0300

    Added SignUp page styles and navigation

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -7,6 +7,7 @@ import {
   Platform
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -17,6 +18,8 @@ import { Container, Title, ForgotPassword, ForgotPasswordText, CreateAccountButt
 
 
 const SignIn: React.FC = () => {
+  const navigation = useNavigation();
+
   return (
     <>
       <KeyboardAvoidingView
@@ -47,7 +50,7 @@ const SignIn: React.FC = () => {
         </ScrollView>
       </KeyboardAvoidingView>
 
-      <CreateAccountButton onPress={() => {}}>
+      <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
         <Icon name="log-in" size={20} color="#ff9000" />
         <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
       </CreateAccountButton>
